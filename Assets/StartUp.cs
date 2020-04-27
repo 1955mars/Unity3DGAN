b50cb21ecb7ba4b545330661cefe040664713b04
Mansoor Patan <1955mars@gmail.com>
2020-04-26 22:28:51 -0400

    Unity3D project created and networks.dll added as plugin

---
+++ Assets/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class StartUp : MonoBehaviour
+{
+    
+    [DllImport("networks")] private static extern void InitNetwork();
+    [DllImport("networks")] private static extern void ApplyNetwork(ref float data, ref float output);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitNetwork();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SomeFunction()
+    {
+        float[] input = new float[1 * 3 * 64 * 64];
+        float[] output = new float[1 * 5 * 64 * 64];
+
+        // Load input with whatever data you want
+        for(int i=0; i<input.Length; i++)
+        {
+            input[i] = 0;
+        }
+
+	    ApplyNetwork(ref input[0], ref output[0]);
+
+
+        // Do whatever you want with the output
+    }
+}
